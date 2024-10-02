034fcb627a8ae56ac4b775875afe5152fff4710e
Pal68 <vpalaguto@yandex.ru>
2024-10-02 20:46:06 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+count_lessons=12
+count_hours=1.5
+course_name="Python"
+time_per_lesson=count_hours/count_lessons
+print("Курс: ",course_name,", всего задач: ", count_lessons, ", затрачено часов: ", count_hours, ", среднее время выполнения ", time_per_lesson, " часа")
